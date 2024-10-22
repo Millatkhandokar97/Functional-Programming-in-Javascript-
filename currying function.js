179e48b3bbd86bf05d4df72e930085e4bfdf0b57
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-23 03:37:50 +0600

    Currying in Javascript

---
+++ currying function.js
@@ -0,0 +1,17 @@
+//Currying in Javascript
+
+function add(a, b, c){
+    return a + b + c
+}
+console.log(add(10, 20, 30));
+
+function currying(a){
+    return function(b){
+        return function(c){
+            return a + b + c
+        }
+    }
+}
+
+let result = currying(10)(20)(30)
+console.log(result);
